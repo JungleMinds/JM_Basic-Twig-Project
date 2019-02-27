6fcd1ebdbbd2386c2933442cf70054acdc1eb09e
grumpyoldman-io <hello@grumpyoldman.io>
2019-02-27 17:38:09 +0100

    Added HTML panel to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,20 @@
-import { configure } from "@storybook/html";
+import { configure, addDecorator } from '@storybook/html'
+import { withKnobs } from '@storybook/addon-knobs'
+import { withHTML } from './utils/html'
+
+addDecorator(withHTML())
+
+addDecorator(withKnobs())
+
+addDecorator((story, config) => {
+  window.dispatchEvent(new Event('reload'))
+  return story()
+})
 
 // automatically import all files ending in *.stories.js
-const req = require.context("../stories", true, /.stories.js$/);
+const req = require.context('../stories', true, /.stories.js$/)
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  req.keys().forEach(filename => req(filename))
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,26 +1,17 @@
-import { storiesOf, setAddon } from "@storybook/html";
-import { withKnobs, text } from "@storybook/addon-knobs";
-
-// setAddon(knobsAddon);
+import { storiesOf } from '@storybook/html'
+import { text } from '@storybook/addon-knobs'
 
 // load app
-import "../src/app";
+import '../src/app'
 
 // load components
-import icon from "../src/components/icon/icon.twig";
-import button from "../src/components/button/button.twig";
+import icon from '../src/components/icon/icon.twig'
+import button from '../src/components/button/button.twig'
 
-const stories = storiesOf("Demo", module);
-
-stories.addDecorator(withKnobs);
-
-stories.addDecorator((story, config) => {
-  window.dispatchEvent(new Event("reload"));
-  return story();
-});
+const stories = storiesOf('Demo', module)
 
 stories
-  .add("icon", () => icon({ label: "i" }))
-  .add("button", () =>
-    button({ arr: [1, 2, 3, 4, text("rilli", "testText")] })
-  );
+  .add('icon', () => icon({ label: 'i' }))
+  .add('button', () =>
+    button({ arr: [1, 2, 3, 4, text('example', 'testText')] })
+  )
